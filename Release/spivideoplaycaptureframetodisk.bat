4cf65768d0e491f2f385c8cfe961bfb29f84357f
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-13 17:58:31 -0500

    first commit

---
+++ Release/spivideoplaycaptureframetodisk.bat
@@ -0,0 +1,2 @@
+start spivideoplaycaptureframetodisk.exe "baraka_chickens(1).avi" -1 100 100 1820 980 220 1 0 0
+rem start spivideoplaycaptureframetodisk.exe "baraka_african-ritual.avi" -1 0 0 1820 980 220 1 1 0
